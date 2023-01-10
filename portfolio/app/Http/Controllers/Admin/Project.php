68a88effb51d8f1ac60c1182e41a1b3fba5aa684
LuongoAndrea <andrea.luongo2002@gmail.com>
2023-01-11 00:55:51 +0100

    controller

--- portfolio/app/Http/Controllers/Admin/Project.php
+++ portfolio/app/Http/Controllers/Admin/Project.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
-class Project extends Controller
+class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
